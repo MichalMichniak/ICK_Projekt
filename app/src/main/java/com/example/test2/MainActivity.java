65299bcf862535a1653958b42d5269bab6a40584
MichalMichniak <michalmichniak2001@gmail.com>
2024-04-27 20:15:42 +0200

    update

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -6,138 +6,102 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.*;
-import io.socket.emitter.Emitter;
-//import io.socket.client.Socket;
-import io.socket.client.IO;
-import java.net.URISyntaxException;
-import java.lang.Math;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
     private SensorManager sensorManager;
     private Sensor sensor;
-
+    // mutex na dane
     private ReentrantLock data_lock;
-
+    // najnowsze dane zakcelerometru
     private float[] data_to_send;
-
+    // flaga czy dane się zmieniły
     private boolean new_data_flag;
-//    private Socket mSocket;
-
-//    {
-//        try {
-//            mSocket = IO.socket("http://192.168.56.1:5050");
-//        } catch (URISyntaxException e) {}
-//    }
     private Socket clientSocket;
-    private float FIR_x[];
-    private float FIR_y[];
-    private float FIR_z[];
-    private int next_idx = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//
-//        StrictMode.setThreadPolicy(policy);
-        FIR_x = new float[6];
-        FIR_y = new float[6];
-        FIR_z = new float[6];
+        // inicjalizacja zmiennych
         data_to_send = new float[3];
         data_lock = new ReentrantLock();
         new_data_flag = false;
-//        try {
-//            InetAddress addr = InetAddress.getByName("localhost");
-//            clientSocket = new Socket("192.168.56.1", 5050);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-        // sockets
+        // nowy wątek socketa inicjalizacja i uruchomienie
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
+                    // inicjalizacja socket-a
                     try {
 //                        clientSocket = new Socket("192.168.0.66", 5050);
-                        clientSocket = new Socket("172.20.10.7", 5050);                    }//
+                        clientSocket = new Socket("192.168.0.66", 5050);                    }//
                     catch (UnknownHostException e) {
-
                         throw new RuntimeException(e);
                     } catch (IOException e) {
-
                         throw new RuntimeException(e);
                     }
                     try {
-//                        OutputStream output = clientSocket.getOutputStream();
-//                        String inputString = "Hello!!!!!";
-//                        byte[] data = inputString.getBytes();
-//                        int len_b = data.length;
-//
-//                        String len_s = Integer.toString(len_b);
-//                        while(len_s.length() > 64) len_s = len_s + " ";
-//                        byte[] header = len_s.getBytes();
-//                        output.write(header);
-//                        output.write(data);
                         try{
+                            // główna pętla wysyłania danych
                             while(true) {
                                 float temp[] = new float[3];
                                 boolean data_flag = false;
+                                // czekanie na mutexa
                                 data_lock.lock();
                                 if(new_data_flag) {
+                                    // przepisanie danych
                                     temp[0] = data_to_send[0];
                                     temp[1] = data_to_send[1];
                                     temp[2] = data_to_send[2];
                                     data_flag = true;
                                     new_data_flag = false;
                                 }
+                                // odblokowanie mutexa
                                 data_lock.unlock();
+                                // jeżeli są nowe dane:
                                 if(data_flag) {
+                                    // inicjalizacja buffora
                                     OutputStream output2 = clientSocket.getOutputStream();
                                     String inputString2 = Float.toString(temp[0]) + "," + Float.toString(temp[1]) + "," + Float.toString(temp[2]);
                                     byte[] data2 = inputString2.getBytes();
+                                    // wartość header-a (ilość bajtów do przesłania)
                                     int len_b2 = data2.length;
-
                                     String len_s2 = Integer.toString(len_b2);
+                                    // padowanie header-a do 64 bajtów
                                     while (len_s2.length() > 64) len_s2 = len_s2 + " ";
                                     byte[] header2 = new byte[64];
                                     byte[] new_len = len_s2.getBytes();
+                                    // przepisanie stringa do tablicy bajtów
                                     for (int i = 0; i < new_len.length; i++) {
                                         header2[i] = new_len[i];
                                     }
+                                    // dopełnienie reszty bajtów spacjami
                                     for (int i = new_len.length; i < 64; i++) {
                                         header2[i] = 32;
                                     }
+                                    // zamiana danych na bajty
                                     data2 = inputString2.getBytes();
-
+                                    // wysyłanie headera i danych przez socket do serwera
                                     output2.write(header2);
                                     output2.write(data2);
+                                    // resetowanie flagi
                                     data_flag = false;
                                 }else{
+                                    // opóźnienie 10 ms
                                     Thread.sleep(10);
                                 }
 
                             }
                         }finally {
-                            // Disconect
+                            // Procedura rozłączania
                             OutputStream output2 = clientSocket.getOutputStream();
-                            String inputString2 = "DISCONECT!";
+                            String inputString2 = "DISCOqNECT!";
                             byte[] data2 = inputString2.getBytes();
                             int len_b2 = data2.length;
 
@@ -158,11 +122,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                             clientSocket.close();
                         }
 
-
-
-
-
-
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
@@ -175,24 +134,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }).start();
 
 
-
+        // dodanie sensora akcelerometru do SensorManager-a
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        final int[] temp = {0};
         setContentView(R.layout.activity_main);
-        ((TextView)findViewById(R.id.textid1)).setText("test");
-
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        ((Button)findViewById(R.id.button_test)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                temp[0]++;
-                ((TextView)findViewById(R.id.textid1)).setText("test"+ temp[0]);
-            }
-        });
-//        mSocket.connect();
-
 
     }
     private EditText mInputMessageView;
@@ -202,9 +148,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (TextUtils.isEmpty(message)) {
             return;
         }
-
         mInputMessageView.setText("");
-//        mSocket.emit("new message", message);
     }
 
     public void onAccuracyChanged(Sensor s, int t){
@@ -212,51 +156,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
     public void onSensorChanged(SensorEvent event) {
         String text = "";
-
         if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-            String text_lr = "";
-//            FIR_x[next_idx] = event.values[0];
-//            FIR_y[next_idx] = event.values[1];
-//            FIR_z[next_idx] = event.values[2];
-//            next_idx=(next_idx+1)%6;
-//
-//            float sum_x = 0;
-//            float sum_y = 0;
-//            float sum_z = 0;
-//            for(int i = 0; i<6; i++) {
-//                sum_x += FIR_x[i];
-//                sum_y += FIR_y[i];
-//                sum_z += FIR_z[i];
-//            }
-//            sum_x = sum_x/6;
-//            sum_y = sum_y/6;
-//            sum_z = sum_z/6;
-//            if(sum > 0.7) text_lr = "Prawo";
-//            else if(sum < -0.7) text_lr = "Lewo";
-//            else text_lr = "___";
+            // wyświetlanie odczytów akcelerometru na ekranie
             text = "X: " + Float.toString(event.values[0]) +
                     ", Y: " + Float.toString(event.values[1]) +
                     ", Z: " + Float.toString(event.values[2]);
             setContentView(R.layout.activity_main);
-
+            // mutex na dane
             if (data_lock.tryLock()) {
                 try {
+                    // przepisanie danych
                     data_to_send[0] = event.values[0];
                     data_to_send[1] = event.values[1];
                     data_to_send[2] = event.values[2];
                     new_data_flag = true;
-//                    ((TextView)findViewById(R.id.lr_text)).setText(Float.toString(0.0));
                 } finally {
                     data_lock.unlock();
                 }
             } else {
                 // próbki przepadaja
-                // perform alternative actions
             }
 
             ((TextView)findViewById(R.id.textid1)).setText(text);
-            //((TextView)findViewById(R.id.lr_text)).setText(text_lr);
-            
 
         }
     }
